43007069544e50df5263526382b4703b49ff0896
Rastislav Kassak <kasou.sk@gmail.com>
2017-03-16 16:35:06 +0100

    first flush of deviations

--- app/Main.hs
+++ app/Main.hs
@@ -13,6 +13,7 @@ import qualified Data.ByteString.Lazy.Char8 as C
 import Control.Arrow
 import Data.Angle
 import Data.Maybe
+import Data.Tuple.Curry
 
 data PathVertex = PathVertex { ts :: Int
                              , lon :: Double
@@ -28,15 +29,15 @@ instance FromJSON PathVertex where
 toRadians x = x/180*pi
 
 haversine :: PathVertex -> PathVertex -> Double
-haversine !start !end = let r = 6371000 :: Double
-                            lat1 = toRadians $ lat start
-                            lat2 = toRadians $ lat end
-                            dlat = (/2) $ toRadians $ lat1 - lat2
-                            dlon = (/2) $ toRadians $ lon start - lon end
-                            sqr x = x * x
-                            a = sqr (sin dlat) + cos lat1 * cos lat2 * sqr (sin dlon)
-                            c = 2 * atan2 (sqrt a) (sqrt $ 1 - a)
-                        in r * c
+haversine start end = let r = 6371000 :: Double
+                          lat1 = toRadians $ lat start
+                          lat2 = toRadians $ lat end
+                          dlat = (/2) $ toRadians $ lat1 - lat2
+                          dlon = (/2) $ toRadians $ lon start - lon end
+                          sqr x = x * x
+                          a = sqr (sin dlat) + cos lat1 * cos lat2 * sqr (sin dlon)
+                          c = 2 * atan2 (sqrt a) (sqrt $ 1 - a)
+                      in r * c
 
 linesAsVertices :: [C.ByteString] -> [PathVertex]
 linesAsVertices = fmap decode >>> catMaybes
@@ -52,6 +53,25 @@ triplets :: [a] -> [(a, a, a)]
 triplets l1@(_:l2@(_:l3@(_:_))) = zip3 l1 l2 l3
 triplets _ = []
 
+middlePoint :: PathVertex -> PathVertex -> Int -> PathVertex
+middlePoint vx1 vx3 ts2 = PathVertex { ts = ts2
+                                     , lon = middleVal lon
+                                     , lat = middleVal lat
+                                     , alt = middleVal alt
+                                     }
+    where dts13 = fromIntegral $ ts vx3 - ts vx1
+          dts12 = fromIntegral $ ts2 - ts vx1
+          dts23 = fromIntegral $ ts vx3 - ts2
+          middleVal val = (dts12 * val vx1 + dts23 * val vx3) / dts13
+
+deviations :: [PathVertex] -> [Double]
+deviations = triplets >>> fmap (uncurryN deviation)
+
+deviation:: PathVertex -> PathVertex -> PathVertex -> Double
+deviation vx1 vx2 vx3 = haversine vx2 vxM
+    where ts2 = ts vx2
+          vxM = middlePoint vx1 vx3 ts2
+
 asPrintedLines :: Show a => [a] -> [C.ByteString]
 asPrintedLines = fmap $ C.pack . show
 
@@ -59,6 +79,6 @@ main :: IO ()
 main = C.interact $
     C.lines >>>
     linesAsVertices >>>
-    distances >>>
+    deviations >>>
     asPrintedLines >>>
     C.unlines
