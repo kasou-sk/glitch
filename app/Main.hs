db249cb152cda22a6f85749b3e8478900c4d8fdc
Rastislav Kassak <kasou.sk@gmail.com>
2017-03-24 00:55:08 +0100

    first attempt to really drop glitches

--- app/Main.hs
+++ app/Main.hs
@@ -14,10 +14,11 @@ import qualified Data.ByteString.Lazy.Char8 as C
 import Control.Arrow
 --import Data.Angle
 import Data.Maybe
+import Data.List
 import Data.Tuple.Curry
 import Control.Lens hiding ((.=))
 
-data PathVertex = PathVertex { _ts :: Double
+data PathVertex = PathVertex { _ts :: Int
                              , _lon :: Double
                              , _lat :: Double
                              , _alt :: Double
@@ -106,21 +107,10 @@ haversine start end = let r = 6371000 :: Double
 linesAsVertices :: [C.ByteString] -> [PathVertex]
 linesAsVertices = fmap decode >>> catMaybes
 
---doublets :: [a] -> [(a, a)]
---doublets l1@(_:l2@(_:_)) = zip l1 l2
---doublets _ = []
-
---distances :: [PathVertex] -> [Double]
---distances = fmap (uncurry haversine) . doublets 
-
---triplets :: [a] -> [(a, a, a)]
---triplets l1@(_:l2@(_:l3@(_:_))) = zip3 l1 l2 l3
---triplets _ = []
-
 data InnerItem a = InnerItem { _prev :: a
                              , _self :: a
                              , _next :: a
-                             }
+                             } deriving (Show)
 makeLenses ''InnerItem
 
 --data TransitionEdge a b = TransitionEdge { _item1 :: a
@@ -129,61 +119,131 @@ makeLenses ''InnerItem
 --                                         }
 --makeLenses ''TransitionEdge
 
-asInnerItems :: [a] -> [InnerItem a]
-asInnerItems l1@(_:l2@(_:l3@(_:_))) = zipWith3 (InnerItem) l1 l2 l3
-asInnerItems _ = []
+asInnerItems :: [a] -> Either [a] [InnerItem a]
+asInnerItems l1@(_:l2@(_:l3@(_:_))) = Right $ zipWith3 (InnerItem) l1 l2 l3
+asInnerItems xs = Left xs
 
-middlePoint :: PathVertex -> PathVertex -> Double -> PathVertex
+splitToSegments :: [PathVertex] -> [[PathVertex]]
+splitToSegments [] = []
+splitToSegments xs = ys : splitToSegments zs
+    where (ys,zs) = continuousSpan isTogether xs    
+          isTogether vx1 vx2 = vx2^.ts - vx1^.ts < 60 && distance vx1 vx2 > 0
+
+continuousSpan :: (a -> a -> Bool) -> [a] -> ([a],[a])
+continuousSpan p (x:xs@(y:_))
+    | p x y     = let (ys,zs) = continuousSpan p xs in (x:ys,zs)
+    | otherwise = ([x],xs)
+continuousSpan _ xs = (xs,[])
+
+middlePoint :: PathVertex -> PathVertex -> Int -> PathVertex
 middlePoint vx1 vx3 ts2 = PathVertex { _ts = ts2
                                      , _lon = middleVal lon
                                      , _lat = middleVal lat
                                      , _alt = middleVal alt
                                      }
-    where dts13 = vx3^.ts - vx1^.ts
-          dts12 = ts2 - vx1^.ts
-          dts23 = vx3^.ts - ts2
+    where dts13 = fromIntegral $ vx3^.ts - vx1^.ts
+          dts12 = fromIntegral $ ts2 - vx1^.ts
+          dts23 = fromIntegral $ vx3^.ts - ts2
           middleVal val = (dts23 * vx1^.val + dts12 * vx3^.val) / dts13
 
 
 deviation :: InnerItem PathVertex -> Double
-deviation ii = distance vxSelf vxM
-    where vxSelf = ii^.self
-          vxPrev = ii^.prev
-          vxNext = ii^.next
+deviation iv = distance vxSelf vxM
+    where vxSelf = iv^.self
+          vxPrev = iv^.prev
+          vxNext = iv^.next
           tsSelf = vxSelf^.ts
           vxM = middlePoint vxPrev vxNext tsSelf
 
 angleCos :: InnerItem PathVertex -> Double
-angleCos ii = sphericalAngleCos a b c
+angleCos iv = sphericalAngleCos a b c
     where a = angularDistance vxPrev vxSelf
           b = angularDistance vxSelf vxNext
           c = angularDistance vxPrev vxNext
-          vxPrev = ii^.prev
-          vxSelf = ii^.self
-          vxNext = ii^.next
+          vxPrev = iv^.prev
+          vxSelf = iv^.self
+          vxNext = iv^.next
 
 
 speed :: InnerItem PathVertex -> Double
-speed ii = (distance vxPrev vxSelf + distance vxSelf vxNext) / (vxNext^.ts - vxPrev^.ts)
-    where vxSelf = ii^.self
-          vxPrev = ii^.prev
-          vxNext = ii^.next
+speed iv = (distance vxPrev vxSelf + distance vxSelf vxNext) / fromIntegral (vxNext^.ts - vxPrev^.ts)
+    where vxSelf = iv^.self
+          vxPrev = iv^.prev
+          vxNext = iv^.next
 
 toTuple :: InnerItem PathVertex -> (Int, Double, Double, Double)
-toTuple ii = (tsInt, deviation ii, angleCos ii, speed ii)
-    where tsInt = floor (ii^.self.ts)
+toTuple iv = (iv^.self.ts, deviation iv, angleCos iv, speed iv)
 
 asPrintedLines :: Show a => [a] -> [C.ByteString]
 asPrintedLines = fmap $ C.pack . show
+
+isGlitch :: InnerItem PathVertex -> Bool
+isGlitch iv = glitchFactor iv > 0.1
+
+glitchFactor :: InnerItem PathVertex -> Double
+glitchFactor iv = d*(1+a)*(1+a)/sqrt(s)
+    where d = deviation iv
+          a = angleCos iv
+          s = speed iv
+
+avg :: (a -> Double) -> [a] -> Double
+avg f xs = avg' 0 0 xs
+    where avg' s c [] = s / fromIntegral c
+          avg' s c (x:xs) = avg' (s + f x) (c + 1) xs
+
+dropGlitches :: [InnerItem PathVertex] -> [InnerItem PathVertex]
+dropGlitches (iv1:l2@(iv2:iv3:iv4:ivs))
+    | isGlitch iv2 && isGlitch iv3 = if notDroppingAny then iv1 : dropGlitches l2
+                                                       else dropGlitches oneLess
+    where oneLess = if betterToDrop3rd
+                    then iv1:iv2':iv4':ivs
+                    else iv1':iv3':iv4:ivs
+          notDroppingAny = currentGFAvg < min dropped2ndGFAvg dropped3rdGFAvg
+          betterToDrop3rd = dropped2ndGFAvg  > dropped3rdGFAvg
+          currentGFAvg = avg glitchFactor [iv1,iv2,iv3,iv4]
+          dropped2ndGFAvg = avg glitchFactor [iv1',iv3',iv4]
+          dropped3rdGFAvg = avg glitchFactor [iv1,iv2',iv4']
+          iv1' = iv1&next .~ iv3^.self
+          iv3' = iv3&prev .~ iv1^.self
+          iv2' = iv2&next .~ iv4^.self
+          iv4' = iv4&prev .~ iv2^.self
+
+dropGlitches (iv1:l2@(iv2:iv3:ivs))
+    | isGlitch iv2 = if notDropping then iv1 : dropGlitches l2
+                                    else dropGlitches oneLess
+    where oneLess = iv1':iv3':ivs
+          notDropping = currentGFAvg < dropped2ndGFAvg 
+          currentGFAvg = avg glitchFactor [iv1,iv2,iv3]
+          dropped2ndGFAvg = avg glitchFactor [iv1',iv3']
+          iv1' = iv1&next .~ iv3^.self
+          iv3' = iv3&prev .~ iv1^.self
+
+dropGlitches (iv1:ivs) = iv1 : dropGlitches ivs
+dropGlitches [] = []
+
+skipEmpty toDo [] = []
+skipEmpty toDo xs = toDo xs
+
+devNull _ = Nothing
 
 main :: IO ()
 main = C.interact $
     C.lines >>>
     linesAsVertices >>>
-    asInnerItems >>>
-    map toTuple >>>
-    map (\(t,d,a,s) -> (t,d,a,s,d*(1+a)*(1+a)/sqrt(s))) >>>
-    filter (\(_,_,_,_,e) -> e > 0.1) >>>
+    splitToSegments >>>
+    fmap (
+        asInnerItems >>>
+        either
+            devNull
+            (
+                dropGlitches >>>
+                --map toTuple >>>
+                --map (\(t,d,a,s) -> (t,d,a,s,d*(1+a)*(1+a)/sqrt(s))) >>>
+                --filter (\(_,_,_,_,e) -> e > 0.1) >>>
+                Just
+            )
+    ) >>>
+    catMaybes >>>
     asPrintedLines >>>
     C.unlines
 
